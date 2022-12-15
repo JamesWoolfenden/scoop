f3980762e8a84d4b8d078b748d84e49cd86ca967
James Woolfenden <james.woolfenden@gmail.com>
2022-12-15 16:51:08 +0000

    and yor

--- build.ps1
+++ build.ps1
@@ -1,15 +1,17 @@
 #!/usr/bin/env pwsh
 param (
     [Parameter(Mandatory=$true)]
-    [string]$Target
+    [string]$Target,
+    [string]$Owner="jameswoolfenden"
 )
 
 $wc = [System.Net.WebClient]::new()
-$version=(Invoke-Webrequest "https://api.github.com/repos/jameswoolfenden/$Target/releases/latest"|convertfrom-json).name
+write-host "Invoke-Webrequest "https://api.github.com/repos/$($Owner)/$Target/releases/latest""
+$version=(Invoke-Webrequest "https://api.github.com/repos/$($Owner)/$Target/releases/latest"|convertfrom-json).name
 $pike=get-content -path "$($Target).json" |ConvertFrom-Json
 $pike.version=$version
 $trimmed=$version -replace "v"
-$pike.url="https://github.com/JamesWoolfenden/$($Target)/releases/download/$version/$($Target)_$($trimmed)_windows_amd64.zip#/$($Target).zip"
+$pike.url="https://github.com/$($Owner)/$($Target)/releases/download/$version/$($Target)_$($trimmed)_windows_amd64.zip#/$($Target).zip"
 $FileHash = Get-FileHash -InputStream ($wc.OpenRead($pike.url))
 $pike.hash=$FileHash.Hash
 $pikeJson =$pike|ConvertTo-Json
